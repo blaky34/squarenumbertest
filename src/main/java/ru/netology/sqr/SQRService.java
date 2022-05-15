ee8a594963a78cf01335f1a3062ab95948c25727
Ксения <ksenia@MacBook-Air-Ksenia.local>
2022-05-15 20:45:49 +0300

    first commit

---
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -0,0 +1,14 @@
+package ru.netology.sqr;
+
+public class SQRService {
+    public int squareСalculate(int min, int max) {
+        int count = 0;
+        for (int i = 10; i <= 99; i++) {
+            int x = i * i;
+            if (x > min && x < max) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
